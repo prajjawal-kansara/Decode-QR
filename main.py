a79f83703a0853bea438ce3d72d2e4f2bf2540bc
Prajjawal Kansara <prajjawalkansara@gmail.com>
2024-06-30 12:35:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import cv2
 from pyzbar.pyzbar import decode
 
-input_dir=r'pics'
+input_dir=r'pics'  #pictures path
 
 for j in sorted(os.listdir(input_dir)):
     img = cv2.imread(os.path.join(input_dir, j) )
