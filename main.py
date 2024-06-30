f1d250c6ee6586937e33d219935b0bee147418a6
Plutonium <prajjawalkansara@gmail.com>
2024-06-30 12:07:07 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+from pyzbar.pyzbar import decode
+
+input_dir=r'pics'
+
+for j in sorted(os.listdir(input_dir)):
+    img = cv2.imread(os.path.join(input_dir, j) )
+
+    qr_info = decode(img)
+
+    print(qr_info)
